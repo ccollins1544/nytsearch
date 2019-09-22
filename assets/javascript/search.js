5bb9030f23d485beec3859476cadac4136aa7163
Christopher Collins <chris@ccollins.io>
2019-09-21 19:05:14 -0600

    Updated HTML

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,4 +1,3 @@
-
 /**
  * searchThis
  * @param {string} findme 
@@ -7,53 +6,53 @@
  * @param {integer} endYear 
  */
 function searchThis(findme,howMany="10",startYear="2018",endYear="2019"){
-    var apiKey = "fnXV6mDoynpDZ8Xrq1iTMUXpMGpP7Zpr";
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + findme + "&sort=newest&api-key=" + apiKey;
+  var apiKey = "fnXV6mDoynpDZ8Xrq1iTMUXpMGpP7Zpr";
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + findme + "&sort=newest&api-key=" + apiKey;
+  
+  startYear = parseInt(startYear) + "0101";
+  endYear = parseInt(endYear) + "0101";
+
+  queryURL += "&begin_date=" + startYear;
+  queryURL += "&end_date=" + endYear;
+
+  // DOM ELEMENTS
+  var resultDiv = $("#results");
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function(dataResult) {
+    console.log(dataResult);
+    var docs = dataResult.response.docs;
     
-    startYear = parseInt(startYear) + "0101";
-    endYear = parseInt(endYear) + "0101";
-
-    queryURL += "&begin_date=" + startYear;
-    queryURL += "&end_date=" + endYear;
-
-    // DOM ELEMENTS
-    var resultDiv = $("#results");
-
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function(dataResult) {
-        console.log(dataResult);
-        var docs = dataResult.response.docs;
-        
-        resultDiv.empty();
-        for(var i=0; i < howMany; i++){
-            var title = docs[i].headline.main;
-            var written_by = docs[i].byline.original;
-            console.log("Title: ",title);
-            console.log("By: ",written_by);
-            console.log("----------------------");
+    resultDiv.empty();
+    for(var i=0; i < howMany; i++){
+      var title = docs[i].headline.main;
+      var written_by = docs[i].byline.original;
+      console.log("Title: ",title);
+      console.log("By: ",written_by);
+      console.log("----------------------");
 
 
-            var articleHeadline = $("<h3>").addClass("articleHeadline");
-            var articleNumber = $("<span>").addClass("label label-primary").text(i);
-            title = $("<strong>").text(title);
-            articleHeadline.append(articleNumber,title);
+      var articleHeadline = $("<h3>").addClass("articleHeadline");
+      var articleNumber = $("<span>").addClass("label label-primary").text(i);
+      title = $("<strong>").text(title);
+      articleHeadline.append(articleNumber,title);
 
-            written_by = $("<h5>").text(written_by);
-            
-            var row = $("<div>").addClass("well").attr("id","article-well-"+i);
-            row.append(articleHeadline, written_by);
-            resultDiv.append(row);
-        }
-    });
-    
+      written_by = $("<h5>").text(written_by);
+      
+      var row = $("<div>").addClass("well").attr("id","article-well-"+i);
+      row.append(articleHeadline, written_by);
+      resultDiv.append(row);
+    }
+  });
+  
 };
 
 $(function(){
-    $("#search_button").on('click',function(e){
-        e.preventDefault();
-        var searchTerm = $("#search_term").val();
-        searchThis(searchTerm);
-    }); // END click search_button
+  $("#search_button").on('click',function(e){
+    e.preventDefault();
+    var searchTerm = $("#search_term").val();
+    searchThis(searchTerm);
+  }); // END click search_button
 });
